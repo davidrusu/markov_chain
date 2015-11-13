8557f5506779d4e3fdba15bb79fd30b020126d49
David Rusu <davidrusu.me@gmail.com>
2015-11-13 18:05:08 -0500

    Lots of improvements
    - error messages
    - using Hemingway poem as initial data
    - can click on suggestions to select them
    - refactored update function
    - filter out suggestions that don’t start with the current state
    -

--- main.elm
+++ main.elm
@@ -39,24 +39,64 @@ type alias Model = { data : String
                    , memory : Int
                    , seed : Random.Seed
                    , trainingData : String
+                   , errorMsg : Maybe String
                    }
 
 initTrainingData : String
 initTrainingData = """
-The Project Gutenberg EBook of Les Miserables, by Victor Hugo
+By A Foreigner
 
-This eBook is for the use of anyone anywhere at no cost and with
-almost no restrictions whatsoever.  You may copy it, give it away or
-re-use it under the terms of the Project Gutenberg License included
-with this eBook or online at www.gutenberg.org
+I like Canadians.
+They are so unlike Americans.
+They go home at night.
+Their cigarettes don't smell bad.
+Their hats fit.
+They really believe that they won the war.
+They don't believe in Literature.
+They think Art has been exaggerated.
+But they are wonderful on ice skates.
+A few of them are very rich.
+But when they are rich they buy more horses
+Than motor cars.
+Chicago calls Toronto a puritan town.
+But both boxing and horse-racing are illegal
+In Chicago.
+Nobody works on Sunday.
+Nobody.
+That doesn't make me mad.
+There is only one Woodbine.
+But were you ever at Blue Bonnets?
+If you kill somebody with a motor car in Ontario
+You are liable to go to jail.
+So it isn't done.
+There have been over 500 people killed by motor cars
+In Chicago
+So far this year.
+It is hard to get rich in Canada.
+But it is easy to make money.
+There are too many tea rooms.
+But, then, there are no cabarets.
+If you tip a waiter a quarter
+He says 'Thank you.'
+Instead of calling the bouncer.
+They let women stand up in the street cars.
+Even if they are good-looking.
+They are all in a hurry to get home to supper
+And their radio sets.
+They are a fine people.
+I like them.
+
+Ernest Hemingway
 """
 
-initModel : Model
-initModel = { markovChain = trainMarkovChain <| tokenizeData initTrainingData
-            , memory = 1
-            , trainingData = initTrainingData
-            , data = ""
-            , seed = Random.initialSeed 0 }
+init : Model
+init = { markovChain = trainMarkovChain <| tokenizeData initTrainingData
+       , memory = 1
+       , trainingData = initTrainingData
+       , data = ""
+       , seed = Random.initialSeed 0
+       , errorMsg = Nothing
+       }
 
 suggestionStyle =
   style [ ("backgroundColor", "#aacccc")
@@ -66,8 +106,8 @@ suggestionStyle =
         , ("margin", "3px")
         ]
 
-viewSuggestion : State -> Html.Html
-viewSuggestion state = div [suggestionStyle] [ text state ]
+viewSuggestion : Signal.Address Action -> State -> Html.Html
+viewSuggestion address state = div [suggestionStyle, onClick address (TakeSuggestion state) ] [ text state ]
 
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
@@ -85,14 +125,20 @@ view address model =
         , ("backgroundColor", "#faf7f0")
         , ("display", "inline-block")
         , ("resize", "none")
+        , ("fontSize", "14px")
         ]
+      errorString = case model.errorMsg of
+                      Nothing -> ""
+                      Just s  -> s
   in 
     div [style styles]
           [ div [] [ text <| "CurrentState: " ++ (toString <| currentState model) ]
+          , div [] [ text <| "Prediction State: " ++ (toString <| predictionState model) ]
+          , div [] [ text errorString ]
           , div [] [ button [ onClick address Daydream ] [ text "Daydream" ]
-                   , div [] <| [ span [ style [("display", "inline-block")]] [] ] ++ (List.map viewSuggestion <| topSuggestions 4 model)
+                   , div [] <| [ span [ style [("display", "inline-block")]] [] ] ++ (List.map (viewSuggestion address) <| topSuggestions 100 model)
                    ]
-          , textarea [ style inputStyle, tabindex -1, onInput address Input, onTab address TakeSuggestion, value model.data] []
+          , textarea [ style inputStyle, tabindex -1, onInput address Input, onTab address TakeTopSuggestion, value model.data] []
           , textarea [ style inputStyle, onInput address TrainingDataInput, value model.trainingData] []
           , button [ onClick address TrainMarkovChain ] [ text "Train Markov Chain" ]
     ]
@@ -111,29 +157,67 @@ onTab address action = onWithOptions "keydown" preventDefaultOptions tabDecoder 
 onInput : Signal.Address a -> (String -> a) -> Attribute
 onInput address contentToValue = on "input" targetValue (\str -> Signal.message address (contentToValue str))
       
-type Action = NoOp | TrainMarkovChain | Daydream | Input String | TrainingDataInput String | TakeSuggestion
+type Action = NoOp
+            | TrainMarkovChain
+            | Daydream
+            | Input String
+            | TrainingDataInput String
+            | TakeTopSuggestion
+            | TakeSuggestion State
+
+appendState : Model -> State -> String
+appendState model state =
+  let
+    curState = currentState model
+    seperator = case curState of
+                  initState -> ""
+                  _         -> " "
+    
+    newData = if | endsInSpace model.data -> model.data
+                 | otherwise -> String.slice 0 -(String.length curState) model.data
+
+    data' = String.concat [newData, seperator, state, " "]
+  in
+    data'
+  
+            
+handleAction : Action -> Model -> Model
+handleAction action model =  
+    case action of
+      NoOp -> model
+      TakeSuggestion state ->
+        { model | data <- appendState model state }
+      TakeTopSuggestion ->
+        let
+          data' = case topSuggestion model of
+                    Just state -> appendState model state
+                    Nothing -> model.data
+        in
+          { model | data <- data' }
+      Input string             -> { model | data <- string }
+      TrainingDataInput string -> { model | trainingData <- string
+                                          , markovChain <- trainMarkovChain (tokenizeData string) }
+      TrainMarkovChain         -> { model | markovChain <- trainMarkovChain (tokenizeData model.trainingData) }
+      Daydream                 ->
+        let
+          predictState = predictionState model
+        in
+          case nextState model.seed model.markovChain predictState of
+            (Just token, newSeed) -> { model | data <- appendState model token
+                                             , seed <- newSeed}
+            (Nothing, newSeed)    -> { model | seed <- newSeed}
+
+updateErrorMsg : Model -> Model
+updateErrorMsg model =
+  let
+    problem = (previousState model) ++ " " ++ (currentState model)
+  in
+    case topSuggestion model of
+      Nothing -> { model | errorMsg <- Just <| "I'm Stuck! feed some examples that start with '" ++ problem ++ "'" }
+      Just _  -> { model | errorMsg <- Nothing }
 
 update : Action -> Model -> Model
-update action model =
-  case action of
-    NoOp                     -> model
-    TakeSuggestion           -> let newData = case String.endsWith " " model.data of -- suggestionsForState model.markovChain (currentState model)  of
-                                                False -> String.slice 0 -(String.length <| currentState model) model.data
-                                                True  -> model.data
-                                    seperator = case currentState model of
-                                                  initState -> ""
-                                                  _         -> " "
-                                in
-                                { model | data <- String.concat [newData, seperator, topSuggestion model, " "] }
-    Input string             -> { model | data <- string }
-    TrainingDataInput string -> { model | trainingData <- string }
-    TrainMarkovChain         -> { model | markovChain <- trainMarkovChain (tokenizeData model.trainingData) }
-    Daydream                 -> let curState = currentState model in
-                                  case nextState model.seed model.markovChain <| curState of
-                                    (Just token, newSeed) -> { model | data <- String.concat [ model.data, " ", token ]
-                                                             , seed <- newSeed }
-                                    (Nothing, newSeed)    -> { model | seed <- newSeed
-                                                             , data <- String.concat [model.data, "b"] }
+update action = handleAction action >> updateErrorMsg
 
 updateNextState : State -> Maybe (List (Float, State)) -> Maybe (List (Float, State))
 updateNextState nextState maybeNextStates = case maybeNextStates of
@@ -166,7 +250,7 @@ probability = Random.float 0 1
 -- essentially computes the integral of the probability distribution
 -- e.g. [(0.1, a), (0.5, b), (0.3, c), (0.1, d)] -> [(0.1, a), (0.6, b), (0.9, c), (1, d)]
 -- This makes drawing a random state weighted by this probability distribution much easier.
--- Just generate 0 < p < 1 and take the first state whose cumulative p value is greater than p
+-- Just generate 0 < p < 1 and take the first state where cumulative p value is greater than p
 
 cumulativePValues : List (Float, State) -> List (Float, State)
 cumulativePValues nextStates =
@@ -181,21 +265,23 @@ suggestionsForState mc state = List.map snd <| List.reverse <| List.sortBy fst <
                                                                                     Just nextStates -> nextStates
 
 
-topSuggestion : Model -> State   
-topSuggestion model = case List.head <| suggestions model of
-                        Nothing -> errorState
-                        Just suggestion -> suggestion
+topSuggestion : Model -> Maybe State   
+topSuggestion model = List.head <| suggestions model
 
 topSuggestions : Int -> Model -> List State
 topSuggestions n model = List.take n <| suggestions model
 
+endsInSpace : String -> Bool
+endsInSpace = String.endsWith " "
+                         
 suggestions : Model -> List State
 suggestions model =
-  let curState = currentState model
-      allSuggestions = case suggestionsForState model.markovChain curState  of
-                         [] -> suggestionsForState model.markovChain (previousState model)
+  let predictState = predictionState model
+      curState = currentState model
+      allSuggestions = case suggestionsForState model.markovChain predictState  of
+                         --[] -> suggestionsForState model.markovChain (previousState model)
                          xs -> xs
-  in  allSuggestions
+  in List.filter (String.startsWith curState) allSuggestions
       
 nextState : Random.Seed -> MarkovChain -> State -> (Maybe State, Random.Seed)
 nextState seed mc state = case Dict.get state mc of
@@ -208,7 +294,15 @@ nextState seed mc state = case Dict.get state mc of
                                              
 -- this should return the initState if list is empty
 currentState : Model -> State
-currentState model = if model.data `String.endsWith` " " then initState else nStatesBack 0 model
+currentState model = if endsInSpace model.data then
+                       initState
+                     else
+                       nStatesBack 0 model
+
+predictionState model = if endsInSpace model.data then
+                          nStatesBack 0 model
+                        else
+                          nStatesBack 1 model
                      
 previousState : Model -> State
 previousState model = nStatesBack 1 model
@@ -222,7 +316,7 @@ actions : Signal.Mailbox Action
 actions = Signal.mailbox NoOp
 
 modelSignal : Signal Model
-modelSignal = Signal.foldp update initModel actions.signal
+modelSignal = Signal.foldp update init actions.signal
 
 main : Signal Html.Html
 main = Signal.map (view actions.address) modelSignal
